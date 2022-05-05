6fbdbe5c1bdd754a64a729a76eb4d906f946adbf
AmyWilhoite <amywilhoite@gmail.com>
2022-05-05 02:32:26 -0400

    updates to routes to get delete working

--- controllers/api/projectRoutes.js
+++ controllers/api/projectRoutes.js
@@ -2,30 +2,55 @@ const router = require('express').Router();
 // const { request } = require('express');
 const { Project } = require('../../models');
 const withAuth = require('../../utils/auth');
-const { readAndAppend, readFromFile } = require('../../utils/fsUtils');
+// const { readAndAppend, readFromFile } = require('../../utils/fsUtils');
 
-router.get('/', (req, res) =>
-  readFromFile('./seeds/projectData.json').then((data) => res.json(JSON.parse(data)))
-);
+// router.get('/', (req, res) =>
+//   readFromFile('./seeds/projectData.json').then((data) => res.json(JSON.parse(data)))
+// );
+
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newProject = await Project.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newProject);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// router.get('/', async (req, res) => {
+//   try {
+//     const projectData = await Project.findByPk(req.params.id, {
+//       include: [
+//         {
+//           model: Project,
+//           attributes: ['name'],
+//         },
+//       ],
+//     });
 
 //post working just not rendering new stuff with out running seeds 
-router.post('/', withAuth, async (req, res) => {
-  console.log(req.body);
-  const {name, description} = req.body; 
-  if (req.body) {
-      // try posting 
-      const newProject = {
-        name,
-        description
-      };
+// router.post('/', withAuth, async (req, res) => {
+//   console.log(req.body);
+//   const {name, description} = req.body; 
+//   if (req.body) {
+//       // try posting 
+//       const newProject = {
+//         name,
+//         description
+//       };
 
-      readAndAppend(newProject, './seeds/projectData.json');
+//       readAndAppend(newProject, './seeds/projectData.json');
 
-      res.json(` added successfully 🚀`);
-    } else {
-      res.error('Error in adding Blog Post');
-    }
-});
+//       res.json(` added successfully 🚀`);
+//     } else {
+//       res.error('Error in adding Blog Post');
+//     }
+// });
+
 
 
 router.delete('/:id', withAuth, async (req, res) => {
@@ -38,10 +63,29 @@ router.delete('/:id', withAuth, async (req, res) => {
     });
 
     if (!projectData) {
-      res.status(404).json({ message: 'No project found with this id!' });
+      res.status(404).json({ message: 'Nothing found with this id!' });
       return;
     }
 
+    res.status(200).json(projectData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+  // update a project 
+router.put('/:id', withAuth, async (req, res) => {
+
+  try {
+    const projectData = await Project.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!projectData[0]) {
+      res.status(404).json({ message: 'Nothing with this id!' });
+      return;
+    }
     res.status(200).json(projectData);
   } catch (err) {
     res.status(500).json(err);
--- public/js/profile.js
+++ public/js/profile.js
@@ -17,7 +17,7 @@ const newFormHandler = async (event) => {
     if (response.ok) {
       document.location.replace('/profile');
     } else {
-      alert('Failed to create project');
+      alert('Failed to create post');
     }
   }
 };
@@ -26,14 +26,14 @@ const delButtonHandler = async (event) => {
   if (event.target.hasAttribute('data-id')) {
     const id = event.target.getAttribute('data-id');
 
-    const response = await fetch(`/api/projects`, {
+    const response = await fetch(`/api/projects/${id}`, {
       method: 'DELETE',
     });
 
     if (response.ok) {
       document.location.replace('/profile');
     } else {
-      alert('Failed to delete project');
+      alert('Failed to delete post');
     }
   }
 };
@@ -43,5 +43,5 @@ document
   .addEventListener('submit', newFormHandler);
 
 document
-  .querySelector('.project-list')
+  .querySelector('.btn-danger')
   .addEventListener('click', delButtonHandler);
